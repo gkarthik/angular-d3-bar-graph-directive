f6cd77ca1ae61d7530c914b1d993ba40b028fac1
gkarthik <xraydiffraction@gmail.com>
2015-03-02 13:40:40 +0530

    Changed to line graph. Added transition. Data request sent from controller.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the connectApp
  */
 angular.module('connectApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.dauJSON = [];
+    $http.get('/sample-data/dailyusers.json').
+      success(function(data, status, headers, config) {
+        $scope.dauJSON = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log("error getting dau data.");
+      });
   });
--- app/scripts/directives/timeseries.js
+++ app/scripts/directives/timeseries.js
@@ -13,6 +13,9 @@ angular.module('connectApp')
       restrict: 'E',
       replace: 'true',
       templateNamespace: 'svg',
+      scope: {
+        jsonFile: '='
+      },
       link: function postLink(scope, element, attrs) {
         var w = 600, 
             h = 300,
@@ -22,21 +25,28 @@ angular.module('connectApp')
           .attr('width', w)
           .attr('height', h)
           .style('border', '2px solid #333');
-        d3.json(attrs.jsonFile, function(data){
+        scope.$watch('jsonFile', function(newJSON, oldJSON){      
+          var data = newJSON;
+          var lineData = [];
           for(var t in data){
             data[t].USERS = parseInt(data[t].USERS);
           }
-          var x = d3.time.scale()
+
+          var x;
+          if(data.length > 0 ){
+            x = d3.time.scale()
                     .domain([new Date(data[0].DAY), new Date(data[data.length-1].DAY)])
                     .range([0 , w-(2*mX)]);
+          } else {
+            x = d3.time.scale().domain([]).range([0, w-(2*mX)]);
+          }           
           var xAxis = d3.svg.axis()
                         .scale(x)
                         .orient('bottom');
           svg.append('svg:g')
               .attr('class','xaxis axis') 
-              .attr('transform', 'translate('+mX+', '+parseInt(h-mY)+')')
+              .attr('transform', 'translate('+(mX)+', '+parseInt(h-mY)+')')
               .call(xAxis);
-              console.log(Math.min.apply(0,_.pluck(data, 'USERS') ));
           var y = d3.scale.linear()
                     .domain([ Math.min.apply(0,_.pluck(data, 'USERS') ), Math.max.apply(0, _.pluck(data, 'USERS'))])
                                                                           .range([0 , parseInt(h-(2*mY))]);
@@ -51,20 +61,60 @@ angular.module('connectApp')
           var wrapper = svg.append('svg:g')
                             .attr('translate','transform('+parseInt(mX+10)+',0)');
           
-          wrapper.selectAll('.users').data(data).enter()
-            .append('svg:rect')
+          var bars = wrapper.selectAll('.users').data(data);
+          
+          bars.enter()
+            .append('svg:circle')
             .attr('class','users')
-            .attr('height', function(d){
-              return y(d.USERS);
+            .attr('r', 2)
+            .attr('cx', function(d, i){
+              var cx = mX+x(new Date(d.DAY));
+              if(i > 0){
+                lineData[i-1].x2 = cx;
+              }
+              if(i < data.length-1){
+                lineData.push({x1: cx, y1: 0, x2: 0, y2: 0 });
+              }
+              return cx;
+            }) 
+            .attr('cy', h-mY)
+            .style('fill', 'steelblue');
+          bars.transition().duration(500)
+            .attr('cy', function(d, i){
+              var cY = h-1-mY-y(d.USERS);
+              if(i > 0){
+                lineData[i-1].y2 = cY;
+              }
+              if(i < data.length-1){
+                lineData[i].y1 = cY;
+              }
+              return cY; 
+            });
+          bars.exit().remove(); 
+          var lines = wrapper.selectAll('.data-lines').data(lineData);
+          lines.enter()
+            .append('svg:line')
+            .attr('x1', function(d){
+              return d.x1;
             })
-            .attr("x", function(d){
-              return mX+x(new Date(d.DAY));
+            .attr('x2', function(d){
+              return d.x2;
             })
-            .attr("y", function(d){
-              return h-1-mY-y(d.USERS);
+            .attr('y1', function(d){
+              return h-mY;
             })
-            .attr('width', 10)
-            .style("fill", "steelblue");
+            .attr('y2', function(d){
+              return h-mY;
+            })
+            .style('stroke', 'steelblue');
+          lines.transition().duration(500)
+            .attr('y1', function(d){
+              return d.y1;
+            })
+            .attr('y2', function(d){
+              return d.y2;
+            })
+          lines.exit().remove();
         });
       }
     };
